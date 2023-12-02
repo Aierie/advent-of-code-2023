6d3c8f5a087b6073f752402e36209797ba3b0f97
aierie <hockeu.w@gmail.com>
2023-12-02 22:16:06 +0800

    d1q2

--- src/bin/d1q2/main.rs
+++ src/bin/d1q2/main.rs
@@ -1,5 +1,3 @@
-use nom::FindSubstring;
-
 const TARGETS: [&str; 10] = [
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
@@ -10,35 +8,56 @@ const REVERSED_TARGETS: [&str; 10] = [
 pub fn main() {
     advent_of_code_2023::util::qn(|value| {
         let s = value
+            .trim_end()
             .split("\n")
             .map(|s| {
-                // let first_word = TARGETS
-                //     .iter()
-                //     .enumerate()
-                //     .filter_map(|(index_in_array, t)| {
-                //         s.find_substring(t)
-                //             .map(|char_index| (index_in_array, char_index))
-                //     })
-                //     .min_by(|a, b| a.1.cmp(&b.1));
+                let mut pre = String::new();
+                let mut post = String::new();
+                let mut first_numeric: Option<u32> = None;
+                let mut last_numeric: Option<u32> = None;
+                let mut pre_is_done = false;
+                for c in s.chars() {
+                    if c.is_numeric() {
+                        let digit = c.to_digit(10);
+                        if first_numeric.is_none() {
+                            first_numeric = digit.clone();
+                        }
+                        last_numeric = digit.clone();
+                        pre_is_done = true;
+                        post = String::new();
+                    } else {
+                        if pre_is_done {
+                            post.push(c);
+                        } else {
+                            pre.push(c);
+                        }
+                    }
+                }
 
-                // let last_word = TARGETS
-                //     .iter()
-                //     .enumerate()
-                //     .filter_map(|(index_in_array, t)| {
-                //         s.find_substring(t)
-                //             .map(|char_index| (index_in_array, char_index))
-                //     })
-                //     .min_by(|a, b| a.1.cmp(&b.1));
-                let trimmed = s
-                    .trim_matches(|c: char| !c.is_numeric())
-                    .trim_end_matches(|c: char| !c.is_numeric());
+                if let Some((value, _)) = TARGETS
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(value, t)| pre.find(t).map(|char_index| (value, char_index)))
+                    .min_by(|a, b| a.1.cmp(&b.1))
+                {
+                    first_numeric = Some(value as u32);
+                }
 
-                let iter = trimmed.chars();
-                let reversed = iter.clone().rev();
-                let first = iter.take(1).next().unwrap();
-                let last = reversed.take(1).next().unwrap();
+                let reversed_post = post.chars().rev().collect::<String>();
+                if let Some((value, _)) = REVERSED_TARGETS
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(value, t)| {
+                        reversed_post.find(t).map(|char_index| (value, char_index))
+                    })
+                    .min_by(|a, b| a.1.cmp(&b.1))
+                {
+                    last_numeric = Some(value as u32);
+                }
 
-                return format!("{first}{last}").parse::<u32>().unwrap();
+                return format!("{}{}", first_numeric.unwrap(), last_numeric.unwrap())
+                    .parse::<u32>()
+                    .unwrap();
             })
             .reduce(|acc, n| acc + n)
             .unwrap();
